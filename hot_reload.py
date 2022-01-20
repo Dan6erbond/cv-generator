82c06d7701cc9777e71ec47c8f21bca4eb965a23
RaviAnand Mohabir <moravrav@gmail.com>
2022-01-20 16:47:23 +0100

    ✨ Implement custom ignore file for hot reloading

--- hot_reload.py
+++ hot_reload.py
@@ -2,15 +2,18 @@ import os
 import subprocess
 import sys
 import time
+from typing import Optional
 
 import pathspec
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("Usage: hot_reload.py <file> <args>")
         sys.exit(1)
 
-    file_path = sys.argv[1]
+    args = sys.argv[1:]
+
+    file_path = args.pop(0)
     if not os.path.isfile(file_path):
         print("File not found:", file_path)
         sys.exit(1)
@@ -18,10 +21,19 @@ if __name__ == "__main__":
     dir_path = os.path.abspath(os.path.dirname(file_path))
 
     spec: pathspec.PathSpec = None
-    if os.path.exists(os.path.join(dir_path, ".gitignore")):
+
+    ignore_file = ".gitignore"
+    for idx, arg in enumerate(args):
+        if arg.startswith("--ignore-file="):
+            ignore_file = "=".join(arg.split("=")[1:])
+            args.pop(idx)
+            break
+
+    spec: Optional[pathspec.PathSpec] = None
+    if os.path.exists(os.path.join(dir_path, ignore_file)):
         spec = pathspec.PathSpec.from_lines(
             pathspec.patterns.GitWildMatchPattern,
-            open(os.path.join(dir_path, ".gitignore")),
+            open(os.path.join(dir_path, ignore_file)),
         )
 
     last_mtime: float = None
@@ -31,11 +43,9 @@ if __name__ == "__main__":
             for f in files:
                 if os.path.join(root, f) == os.path.abspath(__file__):
                     continue
-                if os.path.join(root, f) == os.path.abspath(os.path.join(dir_path, ".gitignore")):
-                    continue
-                if ".git" in root:
-                    continue
-                if spec and spec.match_file(os.path.relpath(os.path.join(root, f), dir_path)):
+                if spec and spec.match_file(
+                    os.path.relpath(os.path.join(root, f), dir_path)
+                ):
                     continue
                 mtime = os.stat(os.path.join(root, f)).st_mtime
                 if last_mtime is None:
@@ -45,6 +55,10 @@ if __name__ == "__main__":
                     print("Reloading:", f)
                     last_mtime = mtime
                     should_reload = True
+                if should_reload:
+                    break
+            if should_reload:
+                break
         if should_reload:
-            subprocess.call([sys.executable, file_path, *sys.argv[2:]])
-        time.sleep(1)
+            subprocess.call([sys.executable, file_path, *args])
+        time.sleep(0.5)
